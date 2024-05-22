155ef4de7437f3ad073eef77f3a85b1c7831e65a
lalit <lalit.pratap.naidu@gmail.com>
2024-05-22 12:37:16 -0400

    added rounded button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,10 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Alert } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { FloatingAction } from "react-native-floating-action";
-
-const actions = [
-  {
-    text: "Scan QR Code",
-    // icon: require("./images/ic_accessibility_white.png"),
-    name: "qr_scan",
-    position: 2,
-  },
-];
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { RoundButton } from "@/components/RoundButton";
 
 export default function HomeScreen() {
   return (
@@ -30,46 +21,26 @@ export default function HomeScreen() {
         <ThemedText type="title">Quickpay</ThemedText>
         <HelloWave />
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: "cmd + d", android: "cmd + m" })}
-          </ThemedText>{" "}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Scan QR Code</ThemedText>
-        <FloatingAction
-          actions={actions}
-          onPressItem={(name) => {
-            console.log(`selected button: ${name}`);
-          }}
-        />
-      </ThemedView>
+      <RoundButton
+        icon="qrcode"
+        label="Scan QR Code"
+        onPress={() => Alert.alert("Scan QR code button pressed")}
+      />
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
   },
   reactLogo: {
     height: 178,
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
 });
