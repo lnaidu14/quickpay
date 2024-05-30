0471ac972c73391da0fe165b984c262c0ae5e6ef
lalit <lalit.pratap.naidu@gmail.com>
2024-05-30 14:39:17 -0400

    created default profile page

--- components/Views/ProfileView.tsx
+++ components/Views/ProfileView.tsx
@@ -12,20 +12,35 @@ export function ProfileView() {
         <ThemedView style={styles.titleContainer}>
           <ThemedText type="title">Profile</ThemedText>
         </ThemedView>
+
+        <ThemedView style={styles.titleContainer}>
+          <ThemedText type="title">QR Code Fill</ThemedText>
+        </ThemedView>
+
+        <ThemedView style={styles.titleContainer}>
+          <ThemedText type="default">ID: 1</ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.titleContainer}>
+          <ThemedText type="default">Name: Lalit</ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.titleContainer}>
+          <ThemedText type="default">Ph: 123456789</ThemedText>
+        </ThemedView>
       </ParallaxScrollView>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: "#808080",
-    bottom: -90,
-    left: -35,
-    position: "absolute",
-  },
   titleContainer: {
     flexDirection: "row",
     gap: 8,
+  },
+  contentContainer: {
+    flexDirection: "row",
+    gap: 8,
+  },
+  contentText: {
+    fontSize: 10,
   },
 });
